aadea6fd45e2e511bea4c727a1b26bb311264cf0
Ikillnukes <ikillnukes1@gmail.com>
2017-06-11 16:51:35 +0200

    Initial commit

---
+++ SoloWordGame/frmMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SoloWordGame
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.nudNumWords = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResueltos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumWords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Palabra:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero de palabras a generar:";
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(171, 12);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(181, 20);
+            this.txtPalabra.TabIndex = 2;
+            // 
+            // nudNumWords
+            // 
+            this.nudNumWords.Location = new System.Drawing.Point(171, 38);
+            this.nudNumWords.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudNumWords.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudNumWords.Name = "nudNumWords";
+            this.nudNumWords.Size = new System.Drawing.Size(181, 20);
+            this.nudNumWords.TabIndex = 3;
+            this.nudNumWords.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultados:";
+            // 
+            // txtResueltos
+            // 
+            this.txtResueltos.Location = new System.Drawing.Point(12, 102);
+            this.txtResueltos.Multiline = true;
+            this.txtResueltos.Name = "txtResueltos";
+            this.txtResueltos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResueltos.Size = new System.Drawing.Size(340, 321);
+            this.txtResueltos.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GENERAR RESULTADOS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(171, 64);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(181, 20);
+            this.txtSeed.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Seed:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 481);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSeed);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResueltos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudNumWords);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Una sola letra";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumWords)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.NumericUpDown nudNumWords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResueltos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SoloWordGame/frmMain.cs
@@ -0,0 +1,129 @@
+﻿using SoloWordGame.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoloWordGame
+{
+    public partial class frmMain : Form
+    {
+        public static Dictionary<int, List<string>> dicc = new Dictionary<int, List<string>>();
+        public const string pattern = "abcdefghijklnmñopqrstuvxyz";
+        public string[] abcd = pattern.ToCharArray().Select(x => Convert.ToString(x)).ToArray();
+        private static Stopwatch sw;
+        private static Random rng;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+
+            sw.Start();
+            var lines = Resources.diccionario.Split(Environment.NewLine.ToCharArray());
+            foreach (string line in lines)
+            {
+                int len = line.Length;
+                bool exist = dicc.ContainsKey(len);
+
+                var lone = exist ? dicc[len] : new List<string>();
+
+                lone.Add(line);
+
+                if (!exist)
+                    dicc.Add(len, lone);
+                else
+                    dicc[len] = lone;
+            }
+
+            rng = new Random(string.IsNullOrWhiteSpace(txtSeed.Text) ? new Random().Next(int.MaxValue) : Convert.ToInt32(Encoding.Unicode.GetBytes(txtSeed.Text)));
+            sw.Stop();
+
+            Console.WriteLine("Everything loaded in {0} ms!", sw.ElapsedMilliseconds);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+
+            sw.Start();
+            Resolver();
+            sw.Stop();
+
+            Console.WriteLine("Resolved in {0} ms!", sw.ElapsedMilliseconds);
+        }
+
+        private void Resolver()
+        {
+            if (string.IsNullOrWhiteSpace(txtPalabra.Text))
+                return;
+
+            txtResueltos.Text = "";
+
+            string word = txtPalabra.Text.ToLower(), curWord = word, newWord = "";
+            bool found = false;
+            List<string> lista = dicc[word.Length],
+                         resolved = new List<string>();
+
+            resolved.Add(curWord);
+            abcd.Shuffle(rng);
+
+            for (int i = 0; i < nudNumWords.Value; ++i)
+            {
+                for(int j = 0; j < curWord.Length; ++j)
+                {
+                    for(int k = 0; k < abcd.Length; ++k)
+                    {
+                        newWord = curWord.Remove(j, 1).Insert(j, abcd[k]);
+
+                        foreach(string s in lista)
+                            if(!resolved.Contains(s) && newWord == s)
+                            {
+                                resolved.Add(s);
+                                txtResueltos.Text += newWord.ToUpper() + Environment.NewLine;
+
+                                curWord = newWord;
+                                found = true;
+                                break;
+                            }
+
+                        if (found)
+                            break;
+                    }
+
+                    if (found)
+                        break;
+                }
+
+                found = false;
+            }
+        }
+    }
+
+    public static class ListHelpers
+    { //Pa la API
+        public static void Shuffle<T>(this IList<T> list, Random rng)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
